e72e86a5f7e9244f6a5ec573d089d04806a340ab
Itay Gold <itil.gold@gmail.com>
2019-01-05 02:02:24 +0200

    cors support

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const segments = require('../controller/segments')
 const segmentsVolumes = require('../controller/segmentsVolumes')
 
 const routes = (app) => {
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+  })
   // Segments Routes
   app.get('/segments', segments)
   app.get('/segments/volumes', segmentsVolumes)
